4a4972fe27897a7565681910d14c82f90000fffc
thkimura <thalles.kimura@gmail.com>
2024-01-28 23:03:59 -0300

    create navbar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar.component"
+
+const App = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Navbar/>}>
+                <Route path="/signin" element={<h1>Sign in page</h1>} />
+                <Route path="/signup" element={<h1>Sign up page</h1>} />
+            </Route>
+        </Routes>
+
+    )
+}
+
+export default App;
